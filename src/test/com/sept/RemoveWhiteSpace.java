a99e83df34ffef2a257046a65c42cf91a2eea7be
abdul <theabdul97@gmail.com>
2024-10-15 07:45:20 +0530

    fact

--- src/test/com/sept/RemoveWhiteSpace.java
+++ src/test/com/sept/RemoveWhiteSpace.java
@@ -3,7 +3,17 @@ package test.com.sept;
 public class RemoveWhiteSpace {
 
 	public static void main(String[] args) {
-		
+		String inputString = "OneSpace TwoSpaces  ThreeSpaces   FourSpaces    Tab        End";
+		char[] charArray = inputString.toCharArray();
+		String stringWithOutSpace = " ";
+		for (int i = 0; i < charArray.length; i++) {
+			if ((charArray[i] != ' ') && (charArray[i] != '\t')) {
+				stringWithOutSpace = stringWithOutSpace + charArray[i];
+			}
+		}
+		System.out.println("input string :" + inputString);
+		System.out.println("Input String Without Spaces :" + stringWithOutSpace);
+
 	}
 
 }
