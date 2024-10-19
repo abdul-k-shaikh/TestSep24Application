0755188efbfd92798a5e9b2fb5ef9bbd5d8ee7bf
abdul <theabdul97@gmail.com>
2024-10-19 22:30:50 +0530

    tripletRemover

--- src/test/com/oct24/RemoveTriplet.java
+++ src/test/com/oct24/RemoveTriplet.java
@@ -11,9 +11,18 @@ public class RemoveTriplet {
 		int a[] = { 1, 2, 3, 3, 3, 4, 5, 6 };
 		Map<Integer, Long> collect = Arrays.stream(a).boxed()
 				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-		
-		
-		
+
+		int res[] = new int[a.length];
+		int j = 0;
+		for (Map.Entry<Integer, Long> data : collect.entrySet()) {
+			if (data.getValue() >= 2) {
+				res[j++] = data.getKey();
+				res[j++] = data.getKey();
+			} else {
+				res[j++] = data.getKey();
+			}
+		}
+		System.out.println(Arrays.toString(res));
 	}
 
 }
