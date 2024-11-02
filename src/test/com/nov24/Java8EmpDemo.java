4059df7f0a0f8e41836e65c45a5d230d6be19418
abdul <theabdul97@gmail.com>
2024-11-03 00:26:38 +0530

    empJava8

--- src/test/com/nov24/Java8EmpDemo.java
+++ src/test/com/nov24/Java8EmpDemo.java
@@ -16,9 +16,14 @@ public class Java8EmpDemo {
 		empList.add(new Employee(5, 2500));
 		empList.add(new Employee(6, 3500));
 
+		// 1. sorting employee in descending order
+		// 2. fetching top 3 salaried employee just added .limit(3)
+		// 3. Fetch all employees having salary less than 3rd higest salary : for this
+		// just replace .limit with .skip()
 		List<Employee> empSortedList = empList.stream().sorted((o1, o2) -> (int) (o2.getSalary() - o1.getSalary()))
-				.collect(Collectors.toList());
+				.limit(3).collect(Collectors.toList());
 		System.out.println("empSortedList :" + empSortedList);
+
 	}
 
 }
