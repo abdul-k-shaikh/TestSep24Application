e3f697534da002681147802a806d9a457c1a140e
abdul <theabdul97@gmail.com>
2024-11-03 21:14:55 +0530

    StreamExamples

---
+++ src/test/com/nov24/SecondSmallestElement.java
@@ -0,0 +1,24 @@
+package test.com.nov24;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
+public class SecondSmallestElement {
+
+	public static void main(String[] args) {
+		int a[] = { 5, 2, 8, 3, 1 };
+		// 1. Find 2nd smallest number
+		int secondSmallest = Arrays.stream(a).boxed().sorted().skip(1).findFirst()
+				.orElseThrow(() -> new IllegalStateException("Array does not have a second smallest element."));
+		System.out.println("secondSmallest: " + secondSmallest);
+
+		// Find 2nd smallest number in 2nd way
+		Integer secondSmallest1 = Arrays.stream(a).boxed().sorted(Comparator.reverseOrder()).skip(3).limit(1)
+				.findFirst().get();
+		System.out.println("secondSmallest1 :" + secondSmallest1);
+		
+	
+	}
+
+}
