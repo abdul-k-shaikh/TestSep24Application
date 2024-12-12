22805c518301054407b35247ee248ef4e2ad64d1
abdul <theabdul97@gmail.com>
2024-12-12 11:53:59 +0530

    charCount

--- src/test/com/dec24/CharCount.java
+++ src/test/com/dec24/CharCount.java
@@ -1,9 +1,20 @@
 package test.com.dec24;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 public class CharCount {
 
 	public static void main(String[] args) {
 		System.out.println("test");
+		String str = "abdabd";
+		Map<String, Long> charCount = Arrays.stream(str.split(""))
+				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+		System.out.println("charCount:" + charCount);
+
 	}
 
 }
