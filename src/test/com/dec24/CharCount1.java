5da75cb3ebd1d8b452b441d6477f8ebc6ffe9ef7
abdul <theabdul97@gmail.com>
2025-01-01 21:48:08 +0530

    charCount

--- src/test/com/dec24/CharCount1.java
+++ src/test/com/dec24/CharCount1.java
@@ -1,10 +1,16 @@
 package test.com.dec24;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class CharCount1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String str = "abdul";
+		Map<String, Long> collect = Arrays.stream(str.split(""))
+				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+		System.out.println("char count:" + collect);
 	}
-
 }
